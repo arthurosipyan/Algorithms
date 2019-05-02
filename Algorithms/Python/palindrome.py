dc814086ec9518fad65e84662d558efc39f1f833
Arthur Osipyan <arthurosipyan@outlook.com>
2019-05-02 01:55:15 -0400

    update palindrome

--- Algorithms/Python/palindrome.py
+++ Algorithms/Python/palindrome.py
@@ -1,20 +1,18 @@
-# Create a method that returns if a string is a palindrome or not.
-
 text = "racecar"
 text_1 = "basketball"
 
 
 def palindrome(text_input):
-    reverse = ""
     i = 0
     j = -1
     while text_input[i] == text_input[j]:
-        reverse += text_input[j]
         i += 1
         j -= 1
-        if reverse == text_input:
+        if i == len(text_input)-1:
             return text_input + " is a palindrome!"
+
     return text_input + " is not a palindrome!"
 
 
 print(palindrome(text))
+print(palindrome(text_1))
