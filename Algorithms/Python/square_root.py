b85dbd2c5abe094f7711bd13b8200aa9f53b554a
Arthur Osipyan <arthurosipyan@outlook.com>
2019-05-02 02:17:14 -0400

    square root

---
+++ Algorithms/Python/square_root.py
@@ -0,0 +1,14 @@
+# Write a function that returns the square root of a given number, given that it is a whole number.
+
+test_1 = 100
+test_2 = 5
+test_3 = 13
+
+
+def square_root(number):
+    return number**.5
+
+
+print(square_root(test_1))
+print(square_root(test_2))
+print(square_root(test_3))
